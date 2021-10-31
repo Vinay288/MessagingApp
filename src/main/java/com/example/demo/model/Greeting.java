db11dd05b1d3cdeaf08ad8c26f50c6f0be5186e5
Vinay288 <vinayhiremath288@gmail.com>
2021-10-31 23:37:08 +0530

    ADDED: greeting model class

--- src/main/java/com/example/demo/model/Greeting.java
+++ src/main/java/com/example/demo/model/Greeting.java
@@ -1,5 +1,24 @@
 package com.example.demo.model;
 
 public class Greeting {
+private long GreetingId;
+private String name;
+
+public Greeting(long incrementAndGet, String name) {
+	setGreetingId(incrementAndGet);
+	setName(name);
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public long getGreetingId() {
+	return GreetingId;
+}
+public void setGreetingId(long greetingId) {
+	GreetingId = greetingId;
+}
 
 }
