fdebc8b3080d5bfac269d0b8e95b6773f566455c
Vinay288 <vinayhiremath288@gmail.com>
2021-10-31 23:35:16 +0530

    ADDED:Project initialization

---
+++ src/main/java/com/example/demo/controller/GreetingController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Greeting;
+
+@RestController
+public class GreetingController {
+
+}
