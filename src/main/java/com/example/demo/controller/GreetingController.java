260bc1e4e0950eacd8c79ac8307de10b3792532e
Vinay288 <vinayhiremath288@gmail.com>
2021-11-01 18:29:41 +0530

    ADDED:declared greeting services object using autowired annotation

--- src/main/java/com/example/demo/controller/GreetingController.java
+++ src/main/java/com/example/demo/controller/GreetingController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,16 +13,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Greeting;
+import com.example.demo.model.User;
+import com.example.demo.service.IGreetingService;
 
 @RestController
 public class GreetingController {
 	
 private static String template="Hello, %s!";
 private final AtomicLong counter=new AtomicLong();
+@Autowired
+private IGreetingService greetingService;
 
 @GetMapping("/greeting")
 public Greeting greeting(@RequestParam(value="name" ,defaultValue = "World")String name) {
-	return new Greeting(counter.incrementAndGet(),String.format(template, name));
+	User user = new User();
+	user.setFirstName(name);
+	return greetingService.addGreeting(user);
 }
 
 @RequestMapping(value = { "/query" })
@@ -30,13 +37,14 @@ public Greeting sayHi(@RequestParam(value = "fname", defaultValue = "vinay") Str
 	return new Greeting(counter.incrementAndGet(),String.format(template, fname+" "+lname));
 }
 
-@GetMapping("/param/{name}")
-public Greeting parameterName(@PathVariable String name) {
-	return new Greeting(counter.incrementAndGet(),String.format(template, name));}
+@GetMapping("/param/{message}")
+public Greeting parameterName(@PathVariable String message) {
+	return new Greeting(counter.incrementAndGet(),String.format(template, message));
+	}
 
 @PostMapping("/post")
 public Greeting setUser(@RequestBody Greeting greeting) {
-	return new Greeting(counter.incrementAndGet(),String.format(template, greeting.getName()));
+	return new Greeting(counter.incrementAndGet(),String.format(template, greeting.getMessage()));
 }
 @PutMapping("/put/{firstName}")
 public Greeting sayHelloPutMethod(@PathVariable String firstName,@RequestParam(value="lastName",defaultValue="Hiremath") String lastName) {
