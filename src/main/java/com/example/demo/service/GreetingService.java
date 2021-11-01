f655378c0fccff35fcc3e6c838f2dd3d8e504779
Vinay288 <vinayhiremath288@gmail.com>
2021-11-01 21:36:04 +0530

    ADDED: implementation of deleteGreeting method

--- src/main/java/com/example/demo/service/GreetingService.java
+++ src/main/java/com/example/demo/service/GreetingService.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.model.Greeting;
@@ -14,14 +16,15 @@ import com.example.demo.repository.GreetingRepository;
 @Service
 public class GreetingService implements IGreetingService {
 
-	private static String template="Hello, %s!";
-	private final AtomicLong counter=new AtomicLong();
-	
+	private static String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
 	@Autowired
 	private GreetingRepository greetingRepository;
+
 	@Override
 	public Greeting addGreeting(User user) {
-		String message=String.format(template,(user.toString().isEmpty())?"Hello World":user.toString());
+		String message = String.format(template, (user.toString().isEmpty()) ? "Hello World" : user.toString());
 		return greetingRepository.save(new Greeting(counter.incrementAndGet(), message));
 	}
 
@@ -29,6 +32,7 @@ public class GreetingService implements IGreetingService {
 	public Greeting getGreetingById(long id) {
 		return greetingRepository.getById(id);
 	}
+
 	@Override
 	public List<Greeting> getAllGreetings() {
 		return greetingRepository.findAll();
@@ -36,14 +40,21 @@ public class GreetingService implements IGreetingService {
 
 	@Override
 	public Greeting editGreeting(long id, User user) {
-		String message=String.format(template,(user.toString().isEmpty())?"Hello World":user.toString());
-		Optional<Greeting> greetingData  =greetingRepository.findById(id);
-		if(greetingData.isPresent()) {
-			Greeting greeting=greetingData.get();
+		String message = String.format(template, (user.toString().isEmpty()) ? "Hello World" : user.toString());
+		Optional<Greeting> greetingData = greetingRepository.findById(id);
+		if (greetingData.isPresent()) {
+			Greeting greeting = greetingData.get();
 			greeting.setMessage(message);
 			greetingRepository.save(greeting);
 			return greeting;
 		}
 		return null;
 	}
+
+	@Override
+	public ResponseEntity<HttpStatus> deleteGreeting(long id) {
+		greetingRepository.deleteById(id);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
 }
