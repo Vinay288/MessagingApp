2060fab956e307038eb8912cce89fa622af923a4
Vinay288 <vinayhiremath288@gmail.com>
2021-11-01 21:36:26 +0530

    ADDED: method to delete a greetings by id

--- src/main/java/com/example/demo/controller/GreetingController.java
+++ src/main/java/com/example/demo/controller/GreetingController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,40 +20,44 @@ import com.example.demo.service.IGreetingService;
 
 @RestController
 public class GreetingController {
-	
-private static String template="Hello, %s!";
-private final AtomicLong counter=new AtomicLong();
-@Autowired
-private IGreetingService greetingService;
 
-@GetMapping("/greeting")
-public Greeting greeting(@RequestParam(value="firstName",defaultValue = "")String firstName,@RequestParam(value="lastName",defaultValue = "")String lastName) {
-	User user = new User();
-	user.setFirstName(firstName);
-	user.setLastName(lastName);
-	return greetingService.addGreeting(user);
-}
+	private static String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+	@Autowired
+	private IGreetingService greetingService;
 
-@GetMapping("/get/{id}")
-public String getMessageById(@PathVariable Long id) {
-	return greetingService.getGreetingById(id).getMessage();
-	}
-@GetMapping("/getAll")
-public List<Greeting> getMessages() {
-	return greetingService.getAllGreetings();
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "firstName", defaultValue = "") String firstName,
+			@RequestParam(value = "lastName", defaultValue = "") String lastName) {
+		User user = new User();
+		user.setFirstName(firstName);
+		user.setLastName(lastName);
+		return greetingService.addGreeting(user);
 	}
 
-@PutMapping("/edit/{id}")
-public Greeting sayHelloPutMethod(@PathVariable Long id,@RequestParam(value="firstName",defaultValue = "")String firstName,@RequestParam(value="lastName",defaultValue = "")String lastName)
-{
-	User user = new User();
-	user.setFirstName(firstName);
-	user.setLastName(lastName);
-	return greetingService.editGreeting(id,user);
-}
-@PostMapping("/post")
-public Greeting setUser(@RequestBody Greeting greeting) {
-	return new Greeting(counter.incrementAndGet(),String.format(template, greeting.getMessage()));
-}
+	@GetMapping("/get/{id}")
+	public String getMessageById(@PathVariable Long id) {
+		return greetingService.getGreetingById(id).getMessage();
+	}
+
+	@GetMapping("/getAll")
+	public List<Greeting> getMessages() {
+		return greetingService.getAllGreetings();
+	}
+
+	@PutMapping("/edit/{id}")
+	public Greeting editMessage(@PathVariable Long id,
+			@RequestParam(value = "firstName", defaultValue = "") String firstName,
+			@RequestParam(value = "lastName", defaultValue = "") String lastName) {
+		User user = new User();
+		user.setFirstName(firstName);
+		user.setLastName(lastName);
+		return greetingService.editGreeting(id, user);
+	}
+
+	@DeleteMapping("/delete/{id}")
+	public void deleteGreeting(@PathVariable Long id) {
+		greetingService.deleteGreeting(id);
+	}
 
 }
