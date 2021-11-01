c4e317408361bdba0e05b65e6c0d8f95205b9c73
Vinay288 <vinayhiremath288@gmail.com>
2021-11-01 18:30:42 +0530

    ADDED: toString method

--- src/main/java/com/example/demo/model/Greeting.java
+++ src/main/java/com/example/demo/model/Greeting.java
@@ -1,24 +1,39 @@
 package com.example.demo.model;
 
-public class Greeting {
-private long GreetingId;
-private String name;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-public Greeting(long incrementAndGet, String name) {
-	setGreetingId(incrementAndGet);
-	setName(name);
+@Entity
+@Table(name="GREETINGS")
+public class Greeting {
+@Id
+private long id;
+@Override
+public String toString() {
+	return "Greeting [id=" + id + ", message=" + message + "]";
 }
-public String getName() {
-	return name;
+private String message;
+
+public Greeting() {
+	id=0;
+	message="";
 }
-public void setName(String name) {
-	this.name = name;
+public Greeting(long id, String message) {
+	setId(id);
+	setMessage(message);
 }
-public long getGreetingId() {
-	return GreetingId;
+public String getMessage() {
+	return message;
 }
-public void setGreetingId(long greetingId) {
-	GreetingId = greetingId;
+public void setMessage(String message) {
+	this.message = message;
+}
+public long getId() {
+	return id;
+}
+public void setId(long id) {
+	this.id = id;
 }
 
 }
