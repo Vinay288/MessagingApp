9b43f6a73b7699753693ecc0d9e66a153b1bbc9e
Vinay288 <vinayhiremath288@gmail.com>
2021-11-01 19:06:15 +0530

    ADDED: declared getAllMessages Method

--- src/main/java/com/example/demo/service/IGreetingService.java
+++ src/main/java/com/example/demo/service/IGreetingService.java
@@ -1,5 +1,7 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.example.demo.model.Greeting;
@@ -9,4 +11,5 @@ import com.example.demo.model.User;
 public interface IGreetingService {
 Greeting addGreeting(User user);
 Greeting getGreetingById(long id);
+List<Greeting> getAllGreetings();
 }
